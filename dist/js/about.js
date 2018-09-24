82c4540a36f75893e2f1e5e5f4b0dbc4306e029b
David Salazar <david.asal@hotmail.com>
2018-09-23 20:55:01 -0500

    added new service pages

--- dist/js/about.js
+++ dist/js/about.js
@@ -1,133 +1,109 @@
-$(".menu-btn").on( "click", function() {
-  $(this).toggleClass("close");
-  $(".menu").toggleClass("show");
-  $(".menu-nav").toggleClass("show");
-  $(".menu-branding").toggleClass("show");
-  $(".nav-item").each(function(){
-  $(this).toggleClass("show");
+$('.menu-btn').on('click', function() {
+  $(this).toggleClass('close');
+  $('.menu').toggleClass('show');
+  $('.menu-nav').toggleClass('show');
+  $('.menu-branding').toggleClass('show');
+  $('.nav-item').each(function() {
+    $(this).toggleClass('show');
   });
 });
 
-
 // pic and title
 var title = new ScrollMagic.Controller();
-  
-  var titleSlide = new ScrollMagic.Scene({
-    triggerElement: '.title',
-    triggerHook: 0.3
-    
-  })
+
+var titleSlide = new ScrollMagic.Scene({
+  triggerElement: '.title',
+  triggerHook: 0.3
+})
   .setClassToggle('.title', 'title-appear')
-  
+
   .addTo(title);
 
-  var img = new ScrollMagic.Controller();
-  
-  var imgSlide = new ScrollMagic.Scene({
-    triggerElement: '.title',
-    triggerHook: 0.3
-    
-  })
+var img = new ScrollMagic.Controller();
+
+var imgSlide = new ScrollMagic.Scene({
+  triggerElement: '.title',
+  triggerHook: 0.3
+})
   .setClassToggle('.image', 'image-appear')
-  
+
   .addTo(img);
 
 // text appear
 var text = new ScrollMagic.Controller();
 
-$('.text').each(function () {
-  
+$('.text').each(function() {
   var textSlide = new ScrollMagic.Scene({
     triggerElement: this,
-    triggerHook:0.9
-    
+    triggerHook: 0.9
   })
-  .setClassToggle(this, 'text-appear')
-  
-  .addTo(text);
+    .setClassToggle(this, 'text-appear')
 
+    .addTo(text);
 });
 
 // headings
 var heading = new ScrollMagic.Controller();
 
-$('.heading').each(function () {
-  
+$('.heading').each(function() {
   var headingSlide = new ScrollMagic.Scene({
-    triggerElement: this,
-    
+    triggerElement: this
   })
-  .setClassToggle(this, 'heading-appear')
-  
-  .addTo(heading);
+    .setClassToggle(this, 'heading-appear')
 
+    .addTo(heading);
 });
 
 var list = new ScrollMagic.Controller();
 
-  var listSlide = new ScrollMagic.Scene({
-    triggerElement: '.list',
-    
-  })
+var listSlide = new ScrollMagic.Scene({
+  triggerElement: '.list'
+})
   .setClassToggle('.list', 'list-appear')
-  
-  .addTo(list);
 
+  .addTo(list);
 
 // Biopic and title
 var bioImg = new ScrollMagic.Controller();
-  
-  var bioImgSlide = new ScrollMagic.Scene({
-    triggerElement: '.block',
-    
-    
-  })
+
+var bioImgSlide = new ScrollMagic.Scene({
+  triggerElement: '.block'
+})
   .setClassToggle('.image', 'bio-img-appear')
-  
+
   .addTo(bioImg);
 
-  var bioHeadings = new ScrollMagic.Controller();
-  
-  var bioHeadingSlide = new ScrollMagic.Scene({
-    triggerElement: '.block',
-    
-    
-  })
+var bioHeadings = new ScrollMagic.Controller();
+
+var bioHeadingSlide = new ScrollMagic.Scene({
+  triggerElement: '.block'
+})
   .setClassToggle('.bio-headings', 'bio-headings-appear')
-  
+
   .addTo(bioHeadings);
 
+// proccess steps slide
 
-  // proccess steps slide
+var leftsteps = new ScrollMagic.Controller();
 
-  var leftsteps = new ScrollMagic.Controller();
-
-$('.left').each(function () {
-  
+$('.left').each(function() {
   var headingSlide = new ScrollMagic.Scene({
     triggerElement: this,
     triggerHook: 0.7
-    
-    
   })
-  .setClassToggle(this, 'step-appear')
-  
-  .addTo(leftsteps);
+    .setClassToggle(this, 'step-appear')
 
+    .addTo(leftsteps);
 });
 
 var rightsteps = new ScrollMagic.Controller();
 
-$('.right').each(function () {
-  
+$('.right').each(function() {
   var headingSlide = new ScrollMagic.Scene({
     triggerElement: this,
     triggerHook: 0.5
-    
-    
   })
-  .setClassToggle(this, 'step-appear')
-  
-  .addTo(rightsteps);
+    .setClassToggle(this, 'step-appear')
 
+    .addTo(rightsteps);
 });
--- dist/js/service.js
+++ dist/js/service.js
@@ -1,39 +1,66 @@
-$(".menu-btn").on( "click", function() {
-  $(this).toggleClass("close");
-  $(".menu").toggleClass("show");
-  $(".menu-nav").toggleClass("show");
-  $(".menu-branding").toggleClass("show");
-  $(".nav-item").each(function(){
-  $(this).toggleClass("show");
+$('.menu-btn').on('click', function() {
+  $(this).toggleClass('close');
+  $('.menu').toggleClass('show');
+  $('.menu-nav').toggleClass('show');
+  $('.menu-branding').toggleClass('show');
+  $('.nav-item').each(function() {
+    $(this).toggleClass('show');
   });
 });
 
-
 // pic and title
 var title = new ScrollMagic.Controller();
-  
-  var titleSlide = new ScrollMagic.Scene({
-    triggerElement: '.title',
-    triggerHook: 0.3
-    
-  })
+
+var titleSlide = new ScrollMagic.Scene({
+  triggerElement: '.title',
+  triggerHook: 0.3
+})
   .setClassToggle('.title', 'title-appear')
-  
+
   .addTo(title);
 
-  var img = new ScrollMagic.Controller();
-  
-  var imgSlide = new ScrollMagic.Scene({
-    triggerElement: '.title',
-    triggerHook: 0.3
-    
-  })
+var img = new ScrollMagic.Controller();
+
+var imgSlide = new ScrollMagic.Scene({
+  triggerElement: '.title',
+  triggerHook: 0.3
+})
   .setClassToggle('.image', 'image-appear')
-  
+
   .addTo(img);
 
+var asset = new ScrollMagic.Controller();
 
+$('.a-block').each(function() {
+  var aBlockSlide = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 1.1
+  })
+    .setClassToggle(this, 'a-block-appear')
 
+    .addTo(asset);
+});
 
+var fpList = new ScrollMagic.Controller();
 
+$('.fp-list').each(function() {
+  var fpSlide = new ScrollMagic.Scene({
+    triggerElement: this,
+    reverse: false
+  })
+    .setClassToggle(this, 'fp-list-appear')
 
+    .addTo(fpList);
+});
+
+var fiBlock = new ScrollMagic.Controller();
+
+$('.fi-block').each(function() {
+  var fpSlide = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 1
+  })
+    .setClassToggle(this, 'fi-block-appear')
+
+    .addTo(fiBlock);
+});
