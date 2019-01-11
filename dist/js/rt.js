458842462ecb7fcffd90e71004d126218afcbcb0
David Salazar <david.asal@hotmail.com>
2019-01-11 11:33:09 -0600

    I think I finally did itgit add . There was an extra hidden input due to the netlify attribute! please for godsakes work

--- dist/js/rt.js
+++ dist/js/rt.js
@@ -89,7 +89,7 @@ function showQuestion(id) {
 }
 
 // listen for submit
-document.getElementById('quiz').addEventListener('submit', function(e) {
+document.getElementById('quiz').addEventListener('submit', function (e) {
   // hide results
   document.getElementById('answer').style.display = 'none';
   // show loader
@@ -204,7 +204,7 @@ var scene = new ScrollMagic.Scene({
 }).addTo(controller);
 
 // change behaviour of controller to animate scroll instead of jump
-controller.scrollTo(function(newpos) {
+controller.scrollTo(function (newpos) {
   TweenMax.to(window, 0.5, {
     scrollTo: {
       y: newpos
@@ -213,7 +213,7 @@ controller.scrollTo(function(newpos) {
 });
 
 //  bind scroll to anchor links
-$(document).on('click', "[href^='#']", function(e) {
+$(document).on('click', "[href^='#']", function (e) {
   var id = $(this).attr('href');
   if ($(id).length > 0) {
     e.preventDefault();
@@ -518,7 +518,7 @@ function caclRiskProfile() {
   let allAnswers = document.getElementsByTagName('input');
   for (i = 0; i <= 49; i++) {
     if (allAnswers[i].checked) {
-      allAnswers[i + 50].setAttribute('checked', 'checked');
+      allAnswers[i + 51].setAttribute('checked', 'checked');
       document.getElementById('a' + i).classList.add('a-checked');
     }
   }
